ff0d7d98345360879f3a13588691440e73a9f99c
byeuri <byeuri602@digigroove.co.kr>
2015-06-17 16:18:39 +0900

    다양한디자인 디자인템플릿 화살표 오버시 opacity값 변화

--- src/main/webapp/resources/js/designpopup.js
+++ src/main/webapp/resources/js/designpopup.js
@@ -1,119 +1,4 @@
 $(function() {
-    //$("#btnTheme1").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme1').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업1
-    //$("#btnTheme2").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme2').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업2
-    //$("#btnTheme3").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme3').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업3
-    //$("#btnTheme4").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme4').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업4
-    //$("#btnTheme5").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme5').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업5
-    //$("#btnTheme6").click(function(){
-    //    var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
-    //    var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
-    //    var windowWid = $(window).width();
-    //
-    //    if(width<0){ width = 0;}
-    //    if(windowWid<767){
-    //        width = 0;
-    //    }
-    //    $('#PopupTheme6').bPopup({
-    //        easing: 'easeOutBack',
-    //        speed: 500,
-    //        transition: 'slideUp',
-    //        closeClass:'popClose',
-    //        positionStyle: 'fixed',
-    //        follow: [false, false],
-    //        position: [width, height]
-    //    });
-    //}); //다양한디자인팝업6
-
     $(".btn-click").click(function(){
         var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
         var width =  $(window).width() - ($(window).width())/2 - ($('.theme-popup .popup-box').outerWidth())/2;
@@ -130,7 +15,7 @@ $(function() {
         idNumber.find('.theme-popup').animate({'opacity':1,'top':height},400,function(){
 
         });
-    }); //다양한디자인팝업7
+    });
 
     $('.popup-wrap').click(function(){
         var height =  Math.max(0, (($(window).height() - ($(window).height()/2) - $(this).outerHeight()) /2) );
